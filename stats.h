4c160d862ba9dafa7291c745c5bd1eba2fa700ca
hungthanh95 <hungthanh95@gmail.com>
2021-05-09 00:37:32 +0700

    Creating Initial Coursera Repository

---
+++ stats.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material.
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief Header file stats.h
+ *
+ * The stats.h header file needs to include declarations and documentation
+ * for the functions from the stats.c file.
+ *
+ * @author Thanh Le
+ * @date May 8 2021
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+
+#endif /* __STATS_H__ */
